83728211e454986b03259fca70a807a93de75068
Sanket Pandia <sanketpandia@gmail.com>
2021-08-01 20:04:00 +0530

    Added a minor command

--- commands/acars_pirep.js
+++ commands/acars_pirep.js
@@ -20,6 +20,8 @@ module.exports = {
         var reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟'];
         if (!("callsign_patterns" in guildData) || !("discord_callsign" in guildData["callsign_patterns"])) {
             message.channel.send("Looks like bot needs an update. *** Please run >update followed by >magic_time and try again***");
+            message.channel.send(">update");
+            message.channel.send(">magic_time");
             return;
         }
         let callsignPattern = await utils.getLiveCallsign(message, guildData["callsign_patterns"]["discord_callsign"], guildData["callsign_patterns"]["if_callsign"]);
