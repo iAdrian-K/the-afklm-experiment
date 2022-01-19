a875cf925d8bcb4d68276f4dfa0aae2ec3e8a2f8
Sanket Pandia <sanketpandia@gmail.com>
2022-01-19 16:31:31 +0530

    Reinstated the world tour files

---
+++ commands/leaderboard.js
@@ -0,0 +1,26 @@
+"use strict"
+const configUtils = require('../helper_services/config_helper');
+const airtableHelper = require("../helper_services/airtable_helper");
+const utils = require("../helper_services/utils");
+const messageCreator = require('../helper_services/message_creator');
+module.exports = {
+    name: "leaderboard",
+    description: "Get World Tour Leaderboard",
+    async execute(message) {
+        let guildId = message.guild.id;
+        let guildData = await configUtils.loadGuildConfigs(guildId)
+        let logs = await airtableHelper.fetchWorldTourLogs(guildData.afklm_special)
+
+        let pilots = {};
+        logs.forEach(log => {
+            if(pilots.hasOwnProperty(log.callsignId)){
+                pilots[log.callsignId].push(log.routeId)
+            }else{
+                pilots[log.callsignId] = [log.routeId]
+            }
+        })
+        let leaders = await utils.getLeaderboards(pilots)
+        let replyMessage = await messageCreator.createLeaderBoard(leaders);
+        await message.channel.send(replyMessage);
+    }
+} 
