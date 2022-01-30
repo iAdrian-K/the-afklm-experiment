44b3ad773b43bf6d3ed741f14ee1b1c7336859c4
Sanket Pandia <sanketpandia@gmail.com>
2022-01-30 11:55:22 +0530

    Initialised wt command

--- commands/wt.js
+++ commands/wt.js
@@ -1,9 +1,24 @@
 "use strict"
+const messageCreator = require('../helper_services/wt_messages');
+
 
 module.exports = {
     name: "wt",
     description: "Get World Tour Route Info",
     async execute(message) {
+        let splitMessage = message.content.split(' ');
+        let dmFlag = splitMessage.length > 3 && splitMessage[2].toString().toUpperCase() === "DM";
+        console.log(splitMessage);
+        try{if (splitMessage[1].parseInt() > 35){
+            let returnMessage = await messageCreator.invalidLegMessage();
+            await message.channel.send(returnMessage)
+            return 
+        }}
+        catch{
+            let returnMessage = await messageCreator.invalidLegMessage();
 
+            await message.channel.send(returnMessage)
+            return
+        }
     }
 }
--- helper_services/wt_messages.js
+++ helper_services/wt_messages.js
@@ -1,24 +1,47 @@
-const Discord = require('discord.js');
-var utils = require('./utils');
+const Discord = require("discord.js");
+var utils = require("./utils");
 
-exports.createLeaderBoard = async function(leaders){
-    let podiums  = ['🥇', '🥈', '🥉', '4️⃣', '5️⃣' ];
-    let leaderboardMessage = "";
-   
-    for (var counter = 0; counter < leaders.length  && counter < 5; counter++) 
+exports.createLeaderBoard = async function (leaders) {
+  let podiums = ["🥇", "🥈", "🥉", "4️⃣", "5️⃣"];
+  let leaderboardMessage = "";
+
+  for (var counter = 0; counter < leaders.length && counter < 5; counter++) {
+    let pilotName = leaders[counter];
+    leaderboardMessage =
+      leaderboardMessage + `${podiums[counter]} - ${pilotName} \n`;
+  }
+  let dataFields = [];
+  dataFields.push({
+    name: "Podium 👑",
+    value: leaderboardMessage,
+    inline: false,
+  });
+  var leaderboard = new Discord.MessageEmbed()
+    .setTitle(`Live AFKLM World Tour 5 Leaderboards`)
+    .addFields(dataFields)
+    .setAuthor("AFKLM World Tour Team");
+  return leaderboard;
+};
+
+exports.invalidLegMessage = async function () {
+  let dataFields = [
     {
-        let pilotName = leaders[counter]
-        leaderboardMessage = leaderboardMessage + `${podiums[counter]} - ${pilotName} \n`;
-    }
-    let dataFields = [];
-    dataFields.push({
-        name: "Podium 👑",
-        value: leaderboardMessage,
-        inline: false
-    })
-    var leaderboard = new Discord.MessageEmbed()
-        .setTitle(`Live AFKLM World Tour 5 Leaderboards`)
-        .addFields(dataFields)
-        .setAuthor("AFKLM World Tour Team")
-    return leaderboard;
-}
+      name: "Total Legs",
+      value: "35",
+      inline: false,
+    },
+    {
+      name: "Remarks",
+      value: "Add 'dm' at the end of the message to receive flight parameters in a DM which can be directly copied",
+      inline: true,
+    },
+    {
+      name: "Command Format",
+      value: ">wt 5 dm",
+      inline: false,
+    },
+  ];
+
+  let retMessage =  new Discord.MessageEmbed().setTitle(`Invalid command`).addFields(dataFields).setAuthor("AFKLM World Tour Team").setColor("#FF0000");
+  return retMessage;
+};
